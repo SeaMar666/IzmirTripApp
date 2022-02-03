6e9c4fa1f5e7f97b1e68a9f7f0471b87b0a2d632
SeaMar666 <denizaygn1907@gmail.com>
2022-02-03 15:38:10 +0300

    Login Page Added

--- app/src/main/java/com/example/izmirtripapp/LoginActivity.java
+++ app/src/main/java/com/example/izmirtripapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.izmirtripapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
