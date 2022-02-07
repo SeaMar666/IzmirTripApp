ed32500a955cfc8ec11cd111dcc7a78fdd789c27
lgazesin <ilgaz-esin@hotmail.com>
2022-02-07 15:38:40 +0300

    firebase

--- app/src/main/java/com/example/izmirtripapp/RegisterActivity.java
+++ app/src/main/java/com/example/izmirtripapp/RegisterActivity.java
@@ -3,12 +3,50 @@ package com.example.izmirtripapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button  regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.reg_btn);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+
+                reference.child(name).setValue(helperClass);
+            }
+        });
     }
 }
