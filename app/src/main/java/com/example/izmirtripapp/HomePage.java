c6578d9aa4936797cfd6f6be58cd021b48b22a66
SeaMar666 <denizaygn1907@gmail.com>
2022-04-30 16:57:32 +0300

    Home page

--- app/src/main/java/com/example/izmirtripapp/HomePage.java
+++ app/src/main/java/com/example/izmirtripapp/HomePage.java
@@ -1,55 +1,55 @@
 package com.example.izmirtripapp;
-
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.izmirtripapp.Model.PlaceData;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class HomePage extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    RecyclerView recyclerView;
 
-    FirebaseDatabase database;
-    DatabaseReference ref;
-    LinearLayoutManager linearLayoutManager;
-    FirebaseRecyclerAdapter<PlaceData,ViewHolder> firebaseRecyclerAdapter;
-    FirebaseRecyclerOptions<PlaceData> options;
+    RecyclerView recyclerView;
+    ListViewHolder adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-
         recyclerView = findViewById(R.id.popularPlacesRecView);
-        recyclerView.setHasFixedSize(true);
 
-        database = FirebaseDatabase.getInstance();
-        ref = database.getReference("Data");
+        FirebaseRecyclerOptions<PlaceData> options =
+                new FirebaseRecyclerOptions.Builder<PlaceData>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Data"),PlaceData.class)
+                        .build();
 
-        showData();
+        adapter = new ListViewHolder(options);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Izmir Venue Suggestions");
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.home);
@@ -79,56 +79,20 @@ public class HomePage extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 
-    private void showData(){
-
-        options = new FirebaseRecyclerOptions.Builder<PlaceData>().setQuery(ref,PlaceData.class).build();
-
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PlaceData, ViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull ViewHolder viewHolder, int i, @NonNull PlaceData placeData) {
-
-                viewHolder.setDetails(getApplicationContext(),placeData.getTitle(), placeData.getImage());
-            }
-
-            @NonNull
-            @Override
-            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.popular_list,parent,false);
-
-                ViewHolder viewHolder = new ViewHolder(itemView);
-                viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-
-                        Toast.makeText(HomePage.this, "Hi", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onItemLongListener(View view, int position) {
-                        Toast.makeText(HomePage.this, "Hiiii", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
-                return viewHolder;
-            }
-        };
-
-        recyclerView.setLayoutManager(linearLayoutManager);
-        firebaseRecyclerAdapter.startListening();
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
-    }
-
-    protected void  onStart(){
-
+    @Override
+    protected void onStart() {
         super.onStart();
-
-        if(firebaseRecyclerAdapter != null){
-            firebaseRecyclerAdapter.startListening();
-            Log.i("BAŞARILı","aaa");
-        }
-
+        adapter.startListening();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
 }
--- app/src/main/java/com/example/izmirtripapp/Model/PlaceData.java
+++ app/src/main/java/com/example/izmirtripapp/Model/PlaceData.java
@@ -1,26 +1,26 @@
 package com.example.izmirtripapp.Model;
 
 public class PlaceData {
-    private String title;
+
     private String image;
+    private String title;
+    private String name;
 
     public PlaceData() {
 
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getImage() {
         return image;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public String getTitle() {
+        return title;
     }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
